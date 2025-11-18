2d38bd35053ffc74a9d3e4fc060627c824d195ed
leejungkuk <siro5643@gmail.com>
2025-11-18 16:58:44 +0900

    initial commit

---
+++ src/main/java/com/self/ticketreservationproject/TicketReservationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.self.ticketreservationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketReservationProjectApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TicketReservationProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/self/ticketreservationproject/TicketReservationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.self.ticketreservationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketReservationProjectApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
